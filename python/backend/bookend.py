d1d41a740defe37dcbd46788d20cf617951eda01
Michael Uman <muman613@gmail.com>
2018-11-29 09:43:16 -0800

    Added many functions to bookshelf flask app
    * Added /book/<isbn> to provide lookup by ISBN returning JSON
    * Added route to serve static files like images (no_image.jpg)
    * When run stand-alone run on port 8000
    * Added fancy CSS for table

--- python/backend/bookend.py
+++ python/backend/bookend.py
@@ -1,6 +1,7 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, send_from_directory
 # import bookshelf
 from bookshelf import BookShelf, SortBy
+import json
 
 app = Flask(__name__)
 db = BookShelf()
@@ -12,18 +13,39 @@ title = "Bookend Book Inventory"
 @app.route("/")
 @app.route("/index")
 def index():
-    return "Hello world"
+    return redirect("/books")
 
-@app.route("/names/<name>")
-def hello_name(name):
-    return render_template('booklist.html', name=name)
+
+@app.route("/static/<file>")
+def serve_static(file):
+    return send_from_directory("static", file)
+
+
+@app.route("/book/<isbn>")
+def lookup_isbn(isbn):
+    book_list = []
+    db.get_books_to_list(book_list)
+    jsonobj = {}
+
+    for book in book_list:
+        if book.isbn == isbn:
+            jsonobj = {
+                "isbn": book.isbn,
+                "title": book.title,
+                "publisher": book.publisher,
+                "pubdate": book.pubdate,
+                "pages": book.pages,
+                "desc": book.desc,
+            }
+    output = json.dumps(jsonobj, indent=4)
+    return output
 
 @app.route("/books")
 def show_books():
     book_list = []
     db.get_books_to_list(book_list, sortby=SortBy.TITLE)
-    return render_template('booklist.html', book_list=book_list, name="Ronald", title=title)
+    return render_template('booklist.html', book_list=book_list, title=title)
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, port=8000)
